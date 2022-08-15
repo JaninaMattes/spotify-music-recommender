50b8c72e92aee21e411ff61edfe9ffcff9619e19
Janina Mattes <janina.alica.mattes@gmail.com>
2022-08-16 00:11:03 +0200

    src: Update dtos

--- backend/music-player-be/src/app/v1/spotify-music/dto/spotify-Track.dto.ts
+++ backend/music-player-be/src/app/v1/spotify-music/dto/spotify-Track.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsNumber, IsOptional, IsObject } from 'class-validator';
 import { ISpotifyItem } from 'src/libs/types';
+import { ArtistDto } from './artist.dto';
+import { SpotifyImageDto } from './spotify-music-image.dto';
+
 
 @Expose()
 export class SpotifyTrackDto implements ISpotifyItem{
@@ -16,4 +19,24 @@ export class SpotifyTrackDto implements ISpotifyItem{
     @IsString()
     name: string;
 
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    album: string
+
+    @ApiProperty()
+    @IsOptional()
+    @IsObject()
+    artists: ArtistDto[];
+
+    @ApiProperty()
+    @IsOptional()
+    @IsObject()
+    images: SpotifyImageDto[];
+
+    @ApiProperty()
+    @IsOptional()
+    @IsNumber()
+    popularity: number;
+  
 }
